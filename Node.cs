5ebce11054a8047a2e10ed55ed73c4103eed0750
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-25 15:45:09 +0530

    [Akash] Added the program for linked list

---
+++ Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListProgram
+{
+    class Node
+    {
+        Object data;
+        Node next;
+        
+        /// <summary>
+        /// Constructor for the Node class
+        /// </summary>
+        /// <param name="data"></param>
+        public Node(Object data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LinkedListProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding the nodes
+            Node newNode = new Node(56);
+            newNode.next = new Node(70);
+            newNode.next.next = new Node(30);
+        }
+    }
+}
